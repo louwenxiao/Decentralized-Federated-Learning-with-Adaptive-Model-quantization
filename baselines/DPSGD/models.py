dbca407ccacacb3fc5864017babf3fd1e7bfc195
娄文霄 <86142265+louwenxiao@users.noreply.github.com>
2022-06-12 10:00:48 +0800

    Add files via upload

---
+++ baselines/DPSGD/models.py
@@ -0,0 +1,163 @@
+import itertools
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torchvision import models
+
+def create_model_instance(dataset_type):
+    if dataset_type == "CIFAR10":
+        return VGG9()
+    elif dataset_type == "FashionMNIST":
+        return CNNFMNIST()
+    elif dataset_type == "CIFAR100":
+        return ResNet18()
+
+
+
+class VGG9(nn.Module):
+    def __init__(self):
+        super(VGG9, self).__init__()
+        self.conv_layer = nn.Sequential(
+            # Conv Layer block 1
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Conv Layer block 2
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=0.05),
+
+            # Conv Layer block 3
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self._initialize_weights()
+
+        self.fc_layer = nn.Sequential(
+            nn.Dropout(p=0.1),
+            #nn.Linear(4096, 1024),
+            nn.Linear(4096, 512),
+            nn.ReLU(inplace=True),
+            #nn.Linear(1024, 512),
+            nn.Linear(512, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.1),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv_layer(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_layer(x)
+        return x
+    
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Linear)):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(0.5)
+                m.bias.data.zero_()
+
+
+
+class CNNFMNIST(nn.Module):
+    def __init__(self):   # 输入维度，宽度，高度，类别数
+        super(CNNFMNIST,self).__init__()
+
+        self.conv1 = nn.Sequential(        # 卷积1，输出维度8，卷积核为3,步长为1,填充1
+            nn.Conv2d(1,32,5,1,2),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2)
+        )
+
+        self.conv2 = nn.Sequential(        # 卷积2，输出维度16，卷积核为3,步长为1，填充1
+            nn.Conv2d(32,64,5,1,2),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2)
+        )
+
+        self.fc_layer = nn.Sequential(
+            nn.Linear(64*7*7, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self,x):
+        out_conv1 = self.conv1(x)
+        out_conv2 = self.conv2(out_conv1)
+        output = out_conv2.view(-1,7*7*64)
+        output = self.fc_layer(output)
+        return output
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, inchannel, outchannel, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.left = nn.Sequential(
+            nn.Conv2d(inchannel, outchannel, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(outchannel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(outchannel, outchannel, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(outchannel)
+        )
+        self.shortcut = nn.Sequential()
+        if stride != 1 or inchannel != outchannel:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(inchannel, outchannel, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(outchannel)
+            )
+
+    def forward(self, x):
+        out = self.left(x)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+class ResNet(nn.Module):
+    def __init__(self, ResidualBlock, num_classes=100):
+        super(ResNet, self).__init__()
+        self.inchannel = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.layer1 = self.make_layer(ResidualBlock, 64,  2, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride=2)
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)   #strides=[1,1]
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inchannel, channels, stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+def ResNet18():
+    return ResNet(ResidualBlock)
+
